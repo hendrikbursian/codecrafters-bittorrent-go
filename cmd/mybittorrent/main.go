317d5828783ca1a0bc9ff9ed7528a14bf72f9ed3
Hendrik Bursian <hendrikbursian@protonmail.com>
2024-07-17 11:04:26 +0200

    Add peers discovery

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -4,11 +4,21 @@ import (
 	"crypto/sha1"
 	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
+	"net/url"
 	"os"
+	"strconv"
 	"strings"
 
 	bencode "github.com/jackpal/bencode-go" // Available if you need it!
 )
+
+type TrackerResponse struct {
+	FailureReason string `bencode:"failure reason"`
+	Interval      int64  `bencode:"interval"`
+	Peers         string `bencode:"peers"`
+}
 
 type Meta struct {
 	Announce string   `bencode:"announce"`
@@ -54,6 +64,56 @@ func main() {
 		fmt.Printf("Tracker URL: %s\n", meta.Announce)
 		fmt.Printf("Length: %d\n", meta.Info.Length)
 		fmt.Printf("Info hash: %x\n", infoHash)
+		fmt.Printf("Piece Length: %d\n", meta.Info.PieceLength)
+		fmt.Printf("Piece Hashes:\n")
+		for i := 0; i < len(meta.Info.Pieces); i += 20 {
+			fmt.Printf("%x\n", meta.Info.Pieces[i:i+20])
+		}
+
+		u, err := url.Parse(meta.Announce)
+		if err != nil {
+			panic(err)
+		}
+
+		values := url.Values{}
+		values.Add("info_hash", string(infoHash))
+		values.Add("peer_id", "00112233445566778899")
+		values.Add("port", "6881")
+		values.Add("uploaded", "0")
+		values.Add("downloaded", "0")
+		values.Add("left", strconv.Itoa(int(meta.Info.Length)))
+		values.Add("compact", "1")
+
+		u.RawQuery = values.Encode()
+
+		log.Printf("Request: %s\n", u.String())
+
+		resp, err := http.Get(u.String())
+		if err != nil {
+			panic(err)
+		}
+
+		var res TrackerResponse
+		err = bencode.Unmarshal(resp.Body, &res)
+		if err != nil {
+			panic(err)
+		}
+		if res.FailureReason != "" {
+			panic(res.FailureReason)
+		}
+
+		peers := []string{}
+		for i := 0; i < len(res.Peers); i += 6 {
+			port := int(res.Peers[i+4])<<8 + int(res.Peers[i+5])
+			peer := fmt.Sprintf("%d.%d.%d.%d:%d", res.Peers[i], res.Peers[i+1], res.Peers[i+2], res.Peers[i+3], port)
+
+			peers = append(peers, peer)
+		}
+
+		for _, p := range peers {
+			fmt.Println(p)
+		}
+
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
